a08a47344b02fb27e98b71f04a27ada920db7fb1
Miguel Acevedo <acevedomiguel@outlook.com>
2023-01-21 15:36:15 -0800

    Added stick figure

--- pages/stage.js
+++ pages/stage.js
@@ -12,6 +12,7 @@ export default function Stage() {
         />
         {/* Load CSS */}
         <link href="/styles/stage.css" rel="stylesheet" />
+        <link href="/styles/stick.css" rel="stylesheet" />
       </Head>
       <div>
         {/* <h1>Epic Rap Battle</h1> */}
@@ -19,6 +20,30 @@ export default function Stage() {
         {/* <Image id="crowd" src={"/assets/crowd.png"} alt="" fill={true} /> */}
         <img id="crowd" src={"/assets/crowd.png"}/>
         {/* <Image id="crowd" src={"/assets/crowd.png"} alt="" height={500} width={1000} /> */}
+        {/* <img id="person1" src={"/assets/stick_figure.png"}/> */}
+        <div className="people">
+          <div class="wrapper">
+              <div class="head"></div>
+              <div class="torso"></div>
+              <div class="leftarm"></div>
+              <div class="rightarm"></div>
+              <div class="leftleg"></div>
+              <div class="leftfoot"></div>
+              <div class="rightleg"></div>
+              <div class="rightfoot"></div>
+          </div>
+
+          <div class="wrapper">
+              <div class="head"></div>
+              <div class="torso"></div>
+              <div class="leftarm"></div>
+              <div class="rightarm"></div>
+              <div class="leftleg"></div>
+              <div class="leftfoot"></div>
+              <div class="rightleg"></div>
+              <div class="rightfoot"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
