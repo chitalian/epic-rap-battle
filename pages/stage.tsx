3299e72b88f175130529e2992507437d872528cc
Miguel Acevedo <acevedomiguel@outlook.com>
2023-01-21 19:30:51 -0800

    Added lights

--- pages/stage.tsx
+++ pages/stage.tsx
@@ -242,6 +242,18 @@ export default function Stage({
           <img src="https://image.ibb.co/nt0hfq/speaker2.png" alt="" />
         </div>
 
+        {activeSpeaker === "person1" && (
+        <div className="lightL">
+          <img src="https://image.ibb.co/f6Hv0q/lightL.png" alt="" />
+        </div>
+        )}
+
+        {activeSpeaker === "person2" && (
+        <div className="lightR">
+          <img src="https://image.ibb.co/j6Lhfq/lightR.png" alt="" />
+        </div>
+        )}
+
         <div className="caption-container">
           <p className="caption">{caption}</p>
         </div>
