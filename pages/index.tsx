8c6868216c5b5e1d93010644a4ad7c5c4354cfd6
Miguel Acevedo <acevedomiguel@outlook.com>
2023-01-21 19:54:33 -0800

    Changed home page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,30 +16,42 @@ export default function Home() {
   }
 
   return (
-    <div className="max-w-md flex flex-col gap-2 m-10">
-      <input
-        className="rounded bg-transparent w-full border border-fuchsia-500"
-        value={name1}
-        placeholder="Person 1"
-        onChange={(e) => setName1(e.target.value)}
-      />
-      <p>
-        {name1} vs {name2}
-      </p>
-      <input
-        className="rounded bg-transparent w-full border border-fuchsia-500"
-        value={name2}
-        placeholder="Person 2"
-        onChange={(e) => setName2(e.target.value)}
-      />
-      <button
-        className="rounded bg-fuchsia-500 text-white"
-        onClick={() => {
-          setSessionStarted(true);
-        }}
-      >
-        Generate
-      </button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1 className="pt-10 text-6xl font-bold text-center">
+        CHOOSE YOUR RAP BATTLE
+      </h1>
+      <div className="max-w-md flex flex-col gap-2 m-10">
+        <input
+          className="py-2 px-4 rounded bg-transparent w-full border border-fuchsia-500"
+          value={name1}
+          placeholder="Person 1"
+          onChange={(e) => setName1(e.target.value)}
+        />
+        <p>
+          {name1} vs {name2}
+        </p>
+        <input
+          className="py-2 px-4 rounded bg-transparent w-full border border-fuchsia-500"
+          value={name2}
+          placeholder="Person 2"
+          onChange={(e) => setName2(e.target.value)}
+        />
+        <button
+          className="py-2 px-4 rounded bg-fuchsia-500 text-white"
+          onClick={() => {
+            setSessionStarted(true);
+          }}
+        >
+          Generate
+        </button>
+      </div>
     </div>
   );
 }
