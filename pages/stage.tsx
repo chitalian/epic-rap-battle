2f1e1ab6d1fdfa6ec7e603fde9853414e14cc283
Miguel Acevedo <acevedomiguel@outlook.com>
2023-01-21 16:41:59 -0800

    Added state for characters

--- pages/stage.tsx
+++ pages/stage.tsx
@@ -1,7 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Script from 'next/script'
+import { useEffect, useState } from "react";
+
 export default function Stage() {
+
+  const [ activeSpeaker, setActiveSpeaker ] = useState("");
+  const [ rapStart, setRapStart ] = useState(false);
+
+  useEffect(() => {
+    setActiveSpeaker("person2");
+  }, []);
+
   return (
     <div>
       <Head>
@@ -13,6 +24,7 @@ export default function Stage() {
         {/* Load CSS */}
         <link href="/styles/stage.css" rel="stylesheet" />
         <link href="/styles/stick.css" rel="stylesheet" />
+        <Script src="/scripts/smoke.js"></Script>
       </Head>
       <div>
         {/* <h1>Epic Rap Battle</h1> */}
@@ -22,7 +34,7 @@ export default function Stage() {
         {/* <Image id="crowd" src={"/assets/crowd.png"} alt="" height={500} width={1000} /> */}
         {/* <img id="person1" src={"/assets/stick_figure.png"}/> */}
         <div className="people">
-          <div className="wrapper">
+        <div id="person1" className={`wrapper ${activeSpeaker === "person1" ? "active" : ""}`}>
               <div className="head"></div>
               <div className="torso"></div>
               <div className="leftarm"></div>
@@ -33,7 +45,7 @@ export default function Stage() {
               <div className="rightfoot"></div>
           </div>
 
-          <div className="wrapper">
+          <div id="person2" className={`wrapper ${activeSpeaker === "person2" ? "active" : ""}`}>
               <div className="head"></div>
               <div className="torso"></div>
               <div className="leftarm"></div>
