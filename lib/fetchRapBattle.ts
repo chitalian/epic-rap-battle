18ecb86484b5f20aa3bcb94938323b3b49354e5b
Justin <justintorre75@gmail.com>
2023-01-21 18:04:21 -0800

    build

--- lib/fetchRapBattle.ts
+++ lib/fetchRapBattle.ts
@@ -1,17 +1,15 @@
 import { RapVerse } from "../pages/api/getBattle";
 import { BattleRequest } from "./promptBuilder";
+import { Result } from "./result";
 
-export async function fetchRapBattle(
-  req: BattleRequest,
-) {
-  const data:Result<RapVerse, string> = await fetch("/api/getBattle", {
+export async function fetchRapBattle(req: BattleRequest) {
+  const data: Result<RapVerse, string> = await fetch("/api/getBattle", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(req),
-  })
-  .then((res) => res.json())
+  }).then((res) => res.json());
 
-  return data.data
+  return data.data;
 }
